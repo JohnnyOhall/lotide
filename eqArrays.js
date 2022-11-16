48c775066b483245ed3bd427e91c724153c0c558
John O'Halloran <john.ohalloran@telus.com>
2022-11-15 20:51:39 -0500

    completed eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ?
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let index in arr1){ // 
+    if (arr1[index] !== arr2[index]){
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
