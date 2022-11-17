dcb73c1f6962142d9e014ae67f45be803abedfd0
John O'Halloran <john.ohalloran@telus.com>
2022-11-16 21:46:22 -0500

    adding findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  return actual === expected ?
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, value) =>{
+
+  for (const val in obj){
+    if (value === obj[val]){
+      return val
+    }  
+  }
+}
+
+// TEST CASES //
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
