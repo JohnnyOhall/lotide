e0f6ef6487e108fa0edd647079223673aed116d8
John O'Halloran <john.ohalloran@telus.com>
2022-11-16 22:38:03 -0500

    eslint check - added comments

--- eqObjects.js
+++ eqObjects.js
@@ -1,42 +1,44 @@
-const eqArrays = (arr1, arr2) => {
-  for (let index in arr1){ // 
-    if (arr1[index] !== arr2[index] || arr1.length !== arr2.length){
-      return false
+// HELPER FUNCTIONS //
+
+const eqArrays = (arr1, arr2) => { //Check if two arrays are the same
+  for (let index in arr1) { //
+    if (arr1[index] !== arr2[index] || arr1.length !== arr2.length) {
+      return false;
     }
   }
+  return true;
+};
 
-  return true
-}
-
-const assertEqual = (actual, expected) => {
+const assertEqual = (actual, expected) => { //Check if two primitives are the same
   return actual === expected ?
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length){
-    return false
-  } 
+// PROJECT //
 
-  for (const key in object1){
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
-      if (eqArrays(object1[key], object2[key]) === false) {
-        return false
+// Function compares two objects to check if they are exactly the same
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) { // Confirms if objects both contain same amount of keys
+    return false; // returns false if object key length !same
+  }
+
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //checks if true that both keys contain arrays
+      if (eqArrays(object1[key], object2[key]) === false) { //uses helper function eqArrays to see if key arrays are the same
+        return false; // return false if key arrays !same
       }
-    } else if (object1[key] !== object2[key]) {
-      return false
+    } else if (object1[key] !== object2[key]) { //compares object keys primitives to see if the same
+      return false; // return false if key primitives !same
     }
-  } 
+  }
   
-  return true
-}
+  return true; // return true if no falsey statements above, objects are identical
+};
     
 
 
-
+// TEST CASES //
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba),true); // => true
