6ead4c8faf01cc5abffd404398e74db9e0bc01d9
John O'Halloran <john.ohalloran@telus.com>
2022-11-18 12:01:27 -0500

    fixed without

--- without.js
+++ without.js
@@ -1,36 +1,58 @@
 const eqArrays = (arr1, arr2) => {
-  for (let index in arr1){ // 
-    if (arr1[index] !== arr2[index] || arr1.length !== arr2.length){
-      return false
+  for (let index in arr1) { //
+    if (arr1[index] !== arr2[index] || arr1.length !== arr2.length) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = (arr1, arr2) => {
-    return eqArrays(arr1,arr2) ?
-      console.log(`✅✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) :
-      console.log(`🛑🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  };
+  return eqArrays(arr1,arr2) ?
+    console.log(`✅✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) :
+    console.log(`🛑🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
 
+// THIS DIDN'T WORK WITH MULTIPLE VALUES //
+
+// const without = (source, remove) => {
+//   let output = [...source];
+
+//   for (let x = 0; x < remove.length; x++) {
+//     for (let y = 0; y < source.length; y++) {
+//       if (remove[x] === source[y]){
+//         output.splice(y,1)
+//       }
+//     }
+//   }
+
+//   return output
+// }
 
 const without = (source, remove) => {
-  let output = [...source];
+  const output = [];
 
-  for (let x = 0; x < remove.length; x++) {
-    for (let y = 0; y < source.length; y++) {
-      if (remove[x] === source[y]){
-        output.splice(y,1)
-      } 
+  for (const x of source) {
+    let removed = false;
+
+    for (const y of remove) {
+      if (x === y) {
+        removed = true;
+      }
+    }
+
+    if (!removed) {
+      output.push(x);
     }
   }
 
-  return output
-}
+  return output;
+};
 
 const words = ["hello", "world", "lighthouse"];
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-console.log(without(words, ["lighthouse"]))
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log(without(words, ["lighthouse"]));
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log(without(['pig','moose', 'cow', 'dog'], ['moose', 'pig'])); // => ['dog', 'cow']
