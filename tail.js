a67cd779d51d1becd320aee19abb3866be18bad1
John O'Halloran <john.ohalloran@telus.com>
2022-11-21 10:21:29 -0500

    refactored tail

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  return actual === expected ?
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
+const assertEqual = require('./assertEqual');
 
 const tail = arr => {
   return Array.isArray(arr) && arr.length > 0 ?
@@ -12,10 +6,5 @@ const tail = arr => {
     `error!`;
 };
  
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(words));
-
+module.exports = tail
 
