fa78c8f9612929a83a488d51403fec1a4de7d3de
John O'Halloran <john.ohalloran@telus.com>
2022-11-21 10:34:29 -0500

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,5 @@
-const assertEqual = require('../assertEqual');
-
 const eqArrays = (arr1, arr2) => {
-  for (let index in arr1){ // 
+  for (let index in arr1){ 
     if (arr1[index] !== arr2[index] || arr1.length !== arr2.length){
       return false
     }
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,5 +3,4 @@ const assertEqual = require('../assertEqual');
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
