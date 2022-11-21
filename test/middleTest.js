af750a93a0f753de7005b6217f6024caf0090dc9
John O'Halloran <john.ohalloran@telus.com>
2022-11-21 11:51:57 -0500

    refactored middle and tail tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,32 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// TEST CASES //
-let testEven = [1,2,3,4,88,99,7,8,9,10];
-let testOdd = [1,2,3,4,5,6,7];
-let testShort = [1,2];
-let testBlank = [];
-let testArrString = ["hello","lighthouse","labs","yeah"];
-let testNull = null;
 
-console.log(middle(testEven));
-console.log(middle(testOdd));
-console.log(middle(testShort));
-console.log(middle(testBlank));
-console.log(middle(testNull));
-console.log(middle(testArrString));
-
-assertArraysEqual(testEven,[1,2,3,4,88,99,7,8,9,10]);
-assertArraysEqual(testArrString,["hello","lighthouse","labs","yeah"]);
+describe("#middle, based on array length: returns 2 item from middle of array if even or single item if odd", () => {
+  it("Test if [ '88', '89' ] returned from testEven Array: [1,2,3,4,88,99,7,8,9,10]", () => {
+    const testEven = [1,2,3,4,88,99,7,8,9,10];
+    assert.deepEqual(middle(testEven), [88,99]);
+  });
+  it('Test returns 4 when given testOdd array: [1,2,3,4,5,6,7]', () => {
+    const testOdd = [1,2,3,4,5,6,7];
+    assert.equal(middle(testOdd), 4);
+  });
+  it('Test returns [] array if array length is less than 3', () => {
+    const testShort = [1,2];
+    assert.deepEqual(middle(testShort), []);
+  });
+  it('Test returns [] array if array is blank: []', () => {
+    const testBlank = [];
+    assert.deepEqual(middle(testBlank), []);
+  });
+  it('Test returns [] array if array is null: null', () => {
+    assert.deepEqual(middle(null), []);
+  });
+  it('Test returns [] array if array is undefined: [undefined]', () => {
+    assert.deepEqual(middle([undefined]), []);
+  });
+  it('Test returns ["lighthouse", "labs"] array from array of strings: ["hello","lighthouse","labs","yeah"]', () => {
+    const testArrString = ["hello","lighthouse","labs","yeah"];
+    assert.deepEqual(middle(testArrString), ['lighthouse', 'labs']);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(words));
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// console.log(tail(words));
+
+describe("#tail", () => {
+  it("Returns [ 'Lighthouse', 'Labs'] out of words array ['Yo Yo', 'Lighthouse', 'Labs']", () =>{
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), [ "Lighthouse", "Labs"]); 
+  });
+  it("Confirm original array was not modified and contains 3 elements still.", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.lengthOf(words, 3)
+  })
+  it("blank array will return 'error!' msg", () => {
+    const words = [];
+    assert.equal(tail(words), 'error!')
+  })
+});
